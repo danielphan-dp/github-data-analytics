7a4c16ff45d69b07838315192c89390c0f0aca8c
danielphan-dp <danielphan.dptp@gmail.com>
2023-09-29 12:37:05 -0400

    pooled some sample data

--- utils/repos_metadata_retriever.py
+++ utils/repos_metadata_retriever.py
@@ -74,7 +74,7 @@ class ReposMetadataRetriever:
         ))
 
         # Save the results to a JSON file
-        json_file_name = f"./_github_api_results_/results_{start}_{end + 1}.json"
+        json_file_name = f"./_github_api_results_/results_{start}_{end}.json"
         try:
             with open(json_file_name, "a+") as outfile:
                 json.dump(self.repos_metadata, outfile, indent=4)
@@ -106,13 +106,12 @@ if __name__ == "__main__":
     )
 
     # Print the JSON data using the handler
-    repos_metadata_retriever.fetch_metadata(start=0, end=1000)
-
     # repos_metadata_retriever.fetch_metadata(start=0, end=1000)
+
     # repos_metadata_retriever.fetch_metadata(start=1000, end=2000)
     # repos_metadata_retriever.fetch_metadata(start=2000, end=3000)
     # repos_metadata_retriever.fetch_metadata(start=3000, end=4000)
-    # repos_metadata_retriever.fetch_metadata(start=4000, end=5000)
+    repos_metadata_retriever.fetch_metadata(start=4000, end=5000)
     # repos_metadata_retriever.fetch_metadata(start=5000, end=6000)
 
     # TODO: Implement more efficient logic, since the GitHub API is limited to 5000 requests / hour.
